c0bf9dbe32ca5cdad1fd6797036b1266b392c6e4
Maverick-mav01 <sunariaboypawan@gmail.com>
2024-06-06 11:33:50 +0530

    Adding Dto

---
+++ src/main/java/com/rwl/Bit_coin/dto/GameStatsDto.java
@@ -0,0 +1,20 @@
+package com.rwl.Bit_coin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GameStatsDto {
+    private String firstName;
+    private String lastName;
+    private int totalGames;
+    private int systemGeneratedClubs;
+    private int privateClubs;
+    private double completedBitcoins;
+    private double totalWinnings;
+}
