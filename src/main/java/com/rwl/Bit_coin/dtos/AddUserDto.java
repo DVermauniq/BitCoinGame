e24cd416b0c358048b32248392537a75c7bc0afa
AjeetTanwar <ajeet.tanwar95@gmail.com>
2024-06-10 16:46:11 +0530

    user

---
+++ src/main/java/com/rwl/Bit_coin/dtos/AddUserDto.java
@@ -0,0 +1,14 @@
+package com.rwl.Bit_coin.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class AddUserDto {
+    String firstName;
+    String lastName;
+    String phoneNumber;
+    String email;
+}
