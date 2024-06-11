f96634c7157ac8f665186b4a13a4637559e32456
prince <prince35.raj@gmail.com>
2024-06-11 17:59:18 +0530

    ClubStory

---
+++ src/main/java/com/rwl/Bit_coin/service/StoryService.java
@@ -0,0 +1,16 @@
+package com.rwl.Bit_coin.service;
+
+
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.rwl.Bit_coin.entity.Story;
+
+import io.jsonwebtoken.io.IOException;
+
+public interface StoryService {
+
+    Story uploadStatus(Long playerId, MultipartFile file) throws IOException, java.io.IOException;
+	
+
+}
