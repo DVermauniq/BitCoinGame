8f3ea6c4aebcfb4e8d6fd3d9bf52c878be34745b
AjeetTanwar <ajeet.tanwar95@gmail.com>
2024-06-11 16:04:34 +0530

    Notification API

--- src/main/java/com/rwl/Bit_coin/ChatBox/ChatMessageController.java
+++ src/main/java/com/rwl/Bit_coin/ChatBox/ChatMessageController.java
@@ -1,15 +1,16 @@
 package com.rwl.Bit_coin.ChatBox;
 
 import com.rwl.Bit_coin.entity.ChatMessage;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/chat")
 public class ChatMessageController {
 
 
